0869d95c9fc9c63c00e9a268202a6d05df9f7ed2
Herman <hrivas5726@aol.com>
2019-01-16 03:46:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ end
 
 def times_iterator(number_of_times)
   7.times do
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
   end  
 end
 
